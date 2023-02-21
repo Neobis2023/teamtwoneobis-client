5721a25e08692b0cf6753104ad600741d381aa77
Jasper <zhorobaev@gmail.com>
2023-02-21 14:53:12 -0500

    feat: add hover effect for buttons and links

--- src/components/guest/Header/Header.jsx
+++ src/components/guest/Header/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
         </nav>
         <div className="header__lang-login basis-1/6 flex gap-x-2 h-fit items-center">
           <NavLink to='/signin' className={'basis-1/2'}><button className="header__login border py-2 px-6 hover:bg-[#E8E5FF] transition hover:text-[#662D91] bg-[#8860C3] rounded-[20px] w-full text-white">Вход</button></NavLink>
-          <div className="header__lang ml-auto flex gap-2 basis-1/2">
+          <div className="header__lang ml-auto flex gap-2 basis-1/2 justify-end">
             <NavLink to={'/'} className={`${styles.language} hover:cursor-pointer ${language==='РУС' ? `${styles.chosen} text-[#662D91]`: 'text-[rgba(102,_45,_145,_0.6)]'}`} onClick={() => setLanguage('РУС')}>РУС</NavLink>
             <NavLink to={'/'} className={`${styles.language} hover:cursor-pointer ${language==='КЫРГ' ? `${styles.chosen} text-[#662D91]` : 'text-[rgba(102,_45,_145,_0.6)]'}`} onClick={() => setLanguage('КЫРГ')}>КЫРГ</NavLink>
           </div>
