289ef2a66aabdc4fb8e344a5aaef38965ac015dd
begalievn <begaliev.info@gmail.com>
2023-04-20 14:55:24 +0600

    Changes in profile about i18n

--- src/pages/User/components/Profile.js
+++ src/pages/User/components/Profile.js
@@ -9,7 +9,7 @@ import { useTranslation } from "react-i18next";
 
 
 const Profile = () => {
-  
+  const { t } = useTranslation();
   const { data, isLoading } = useGetProfileQuery();
   
   console.log(data);
